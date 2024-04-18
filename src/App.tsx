7225f7fc0f5b4880317414cffe8ce92557920565
Kim_dain <ekdls15937@naver.com>
2024-04-18 12:49:37 +0900

    2024.04.18 12:49

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
 import Layout from './react-router/Layout';
 import AxiosLibrary from './axios/AxiosLibrary';
+import ReactCookieLibrary from './react-cookie/ReactCookieLibrary';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       </Route>
       <Route path='/zustand' element={<ZustandLibrary />} />
       <Route path='/axios' element={<AxiosLibrary />}/>
+      <Route path='/cookie/cookie' element={<ReactCookieLibrary />} />
       <Route path='*' element={<h3>404 Error</h3>} /> 
     </Routes>
     
