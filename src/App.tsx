ed642f2df00230f6884d902da8c6987d2b12dfa7
Kim_dain <ekdls15937@naver.com>
2024-04-18 12:13:37 +0900

    2024.04.18 12:13

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
+import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
 
 
 function App() {
@@ -13,12 +15,14 @@ function App() {
       <Route path='/page2' element={<ReactRouterPage2 />} />
       <Route path='/zustand' element={<ZustandLibrary />} /> */}
 
-      <Route path='/router'>
+      <Route path='/router' element={<Layout />}>
         <Route index element={<ReactRouterLibrary />} />
-        <Route path='page1' element={<ReactRouterPage1 />} />
+        <Route path='page1/:value' element={<ReactRouterPage1 />} />
         <Route path='page2' element={<ReactRouterPage2 />} />
       </Route>
       <Route path='/zustand' element={<ZustandLibrary />} />
+      <Route path='/axios' element={<AxiosLibrary />}/>
+      <Route path='*' element={<h3>404 Error</h3>} /> 
     </Routes>
     
   );
